79a75cc45c9344da12e48424430e594f78132baa
Truong Phu Phat Felix (Zyo) <truongphuphat98@yahoo.fr>
2018-10-23 17:59:17 +0200

    Add Pixel Project

---
+++ Pixel_ImageEffect/Assets/Pixels/Scripts/pixelateImageEffector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class pixelateImageEffector : MonoBehaviour {
+
+	public float reduction = 1;
+
+	// Postprocess the image
+	void OnRenderImage (RenderTexture source, RenderTexture destination)
+	{
+		//temp RT attributes
+		RenderTextureFormat format = RenderTextureFormat.ARGB32;
+		int lowresDepthWidth = (int)(source.width/reduction);
+		int lowresDepthHeight = (int)(source.height/reduction);
+
+		//make RT
+		RenderTexture lowresRT = RenderTexture.GetTemporary (lowresDepthWidth, lowresDepthHeight, 0, format);
+
+		//point sampling for crispness sake
+		lowresRT.filterMode = FilterMode.Point;
+
+		//Blit source to lowres RT
+		Graphics.Blit (source, lowresRT);
+		//Blit lowres to screen
+		Graphics.Blit (lowresRT, destination);
+
+		//release lowres RT from memory
+		RenderTexture.ReleaseTemporary(lowresRT);
+	}
+}
---
+++ Pixel_ImageEffect/README.md
@@ -0,0 +1,5 @@
+# Analog_Pixelate
+
+VERY basic pixelation image effect for Unity (All in OnRenderImage without shaders)
+
+![pix](https://cloud.githubusercontent.com/assets/2878395/22015647/e18594d6-dc9a-11e6-9ff5-e52756d876e4.PNG)
